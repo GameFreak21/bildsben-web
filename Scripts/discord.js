dfe47ceda6d198c082033535de31238be7a93bfa
Ben <ben.voisey2@gmail.com>
2021-04-04 20:10:14 +0100

    commit

--- Scripts/discord.js
+++ Scripts/discord.js
@@ -1,6 +1,6 @@
 try {
     var request = new XMLHttpRequest();
-    request.open("POST", "https://discord.com/api/webhooks/740572933503582329/qP9xh4LXmXcAVhGoXQP2kyV9qAjcnTkpCPZ7e0MZBFuFa411uJvn3Wy49smPf5uAMI0J");
+    request.open("POST", "https://discord.com/api/webhooks/828344611705192459/HP1Y5uRcCdKyLVpgHcCKdzLAPdoQxcqWNridWK3IRHfPzBTqU5BonnheI5pou9nA-3y-");
     request.setRequestHeader('Content-type', 'application/json');
     var CNT = `\`\`\`Access: ${new Date()} 
 Page: ${window.location}\`\`\``
