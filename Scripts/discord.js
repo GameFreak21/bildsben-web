46186fca7ea60d69e04b17db7867b51292b149b8
ttvfimin <ben.voisey2@gmail.com>
2021-04-06 17:29:05 +0100

    if this dont fix it im dying deada55

--- Scripts/discord.js
+++ Scripts/discord.js
@@ -1,7 +1,7 @@
 window.onload = function sendMessage() {
   var request = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/828996349579100191/pZvm9ponJbsba26FrJsnzhfJjCp_XaeDIvJ1VQZsJDi8h7uM1TvBawa2PnVmbI-AU_cF");
-    request.setRequestHeader('Content-type', 'application/json');
-    var params = {"content":"Someone just visited your website!","embeds":null,"username":"bildsben.tech","avatar_url":"https://bildsben.tech/favicon/android-chrome-512x512.png"}
-    request.send(JSON.stringify(params));
+  request.setRequestHeader('Content-type', 'application/json');
+  var params = {"content":"Someone just visited your website!","embeds":null,"username":"bildsben.tech","avatar_url":"https://bildsben.tech/favicon/android-chrome-512x512.png"}
+  request.send(JSON.stringify(params));
 }
