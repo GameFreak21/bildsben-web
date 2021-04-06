12d8c63aeaf520f97ddc81901c187b9be62d4b19
Ben <ben.voisey2@gmail.com>
2021-04-06 18:59:09 +0100

    am i smart?

--- Scripts/TypeItDiscIt.js
+++ Scripts/TypeItDiscIt.js
@@ -1,4 +1,3 @@
-
 var TxtType = function (el, toRotate, period, toDelete) {
     this.toRotate = toRotate;
     this.el = el;
@@ -48,6 +47,20 @@ TxtType.prototype.tick = function () {
 };
 
 window.onload = function () {
+	// Discord
+	var request = new XMLHttpRequest();
+	console.log("var request")
+	request.open("POST", "https://discord.com/api/webhooks/828996349579100191/pZvm9ponJbsba26FrJsnzhfJjCp_XaeDIvJ1VQZsJDi8h7uM1TvBawa2PnVmbI-AU_cF");
+	console.log("POSTED")
+	request.setRequestHeader('Content-type', 'application/json');
+	console.log("request headers done")
+	var params = {"content":"Someone just visited your website!","embeds":null,"username":"bildsben.tech","avatar_url":"https://bildsben.tech/favicon/android-chrome-512x512.png"};
+	console.log("params done")
+	request.send(JSON.stringify(params));
+	console.log("sent req")
+
+	// Typewrite
+
     var elements = document.getElementsByClassName('typewrite');
     setTimeout(() => {    
         for (var i = 0; i < elements.length; i++) {
