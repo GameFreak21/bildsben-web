d7240c67ae603f80ed50dc00136bd21198d198b2
Ben <ben.voisey2@gmail.com>
2021-04-06 18:39:17 +0100

    yes

--- Scripts/discord.js
+++ Scripts/discord.js
@@ -1,7 +1,12 @@
 window.onload = function sendMessage() {
   var request = new XMLHttpRequest();
+  console.log("var request")
   request.open("POST", "https://discord.com/api/webhooks/828996349579100191/pZvm9ponJbsba26FrJsnzhfJjCp_XaeDIvJ1VQZsJDi8h7uM1TvBawa2PnVmbI-AU_cF");
+  console.log("POSTED")
   request.setRequestHeader('Content-type', 'application/json');
-  var params = {"content":"Someone just visited your website!","embeds":null,"username":"bildsben.tech","avatar_url":"https://bildsben.tech/favicon/android-chrome-512x512.png"}
+  console.log("request headers done")
+  var params = {"content":"Someone just visited your website!","embeds":null,"username":"bildsben.tech","avatar_url":"https://bildsben.tech/favicon/android-chrome-512x512.png"};
+  console.log("params done")
   request.send(JSON.stringify(params));
+  console.log("sent req")
 }
